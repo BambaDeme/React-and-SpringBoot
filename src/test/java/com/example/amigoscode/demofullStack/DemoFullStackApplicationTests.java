d4063ce2c194636e02ba4ede082b2332886f2df7
bambademe <cheikhbambademe@gmail.com>
2021-09-11 09:00:23 -0700

    initialization front and back

---
+++ src/test/java/com/example/amigoscode/demofullStack/DemoFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amigoscode.demofullStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFullStackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
